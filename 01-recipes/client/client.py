839304e608fc515c56d06cddc1d55bb5a1ba0774
Серебряков М.А. <mikhail@synzr.space>
2026-02-03 13:52:16 +0500

    fix(01-recipes/client): send/recv methods

--- 01-recipes/client/client.py
+++ 01-recipes/client/client.py
@@ -9,21 +9,6 @@ class Client:
         self.__socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.__id = None
 
-    def connect(self):
-        if self.__id:
-            return
-
-        self.__socket.sendto(b"CONNECT\n", ADDRESS)
-
-        message, _ = self.__socket.recvfrom(BUFFER_SIZE)
-        message = message.decode("ascii")
-
-        if message.upper().startswith("ERROR"):
-            code = int(message.split("\n")[0].split(" ")[1].strip(), 10)
-            raise Exception(f"Protocol error with code {code}")
-
-        self.__id = message.split("\n")[1].split(" ")[1].strip()
-
     def __send(self, command):
         if not self.__id:
             raise Exception("Client not connected")
@@ -31,15 +16,31 @@ class Client:
         message = f"ME {self.__id}\n{command}\n"
         self.__socket.sendto(message.encode('ascii'), ADDRESS)
 
-    def ping(self):
-        self.__send("PING")
-
+    def __recv(self):
         message, _ = self.__socket.recvfrom(BUFFER_SIZE)
         message = message.decode("ascii")
 
         if message.upper().startswith("ERROR"):
             code = int(message.split("\n")[0].split(" ")[1].strip(), 10)
             raise Exception(f"Protocol error with code {code}")
+
+        return message
+
+    def connect(self):
+        if self.__id:
+            return
+
+        self.__socket.sendto(b"CONNECT\n", ADDRESS)
+
+        message = self.__recv()
+        self.__id = message.split("\n")[1].split(" ")[1].strip()
+
+    def ping(self):
+        self.__send("PING")
+
+        message = self.__recv()
+        if "PONG" not in message.upper():
+            raise Exception("Server didn't pong")
 
 
 def main():
