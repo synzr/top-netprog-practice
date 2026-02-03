834a81e859a43d4dc281e61064eb81353580b5f0
Серебряков М.А. <mikhail@synzr.space>
2026-02-03 12:46:50 +0500

    feat(01-recipes/server): echo server

--- 01-recipes/server/index.js
+++ 01-recipes/server/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import { createSocket } from "node:dgram";
+
+const PORT = 4884; // NOTE: protocol port
+
+const server = createSocket("udp4");
+
+server.on("listening", () => {
+    console.info("Server started");
+});
+
+server.on("message", (data, remote) => {
+    server.send(data, remote.port, remote.address);
+});
+
+server.bind(PORT, "0.0.0.0");
