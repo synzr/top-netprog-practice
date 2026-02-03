945f52e5f99b359bc1b49d44ff0b3c32c921e2fc
Серебряков М.А. <mikhail@synzr.space>
2026-02-03 13:06:02 +0500

    feat(01-recipes/server): "connect" command

--- 01-recipes/server/index.js
+++ 01-recipes/server/index.js
@@ -1,15 +1,37 @@
+import NodeCache from "node-cache";
+
 import { createSocket } from "node:dgram";
+import crypto from "node:crypto";
 
 const PORT = 4884; // NOTE: protocol port
 
 const server = createSocket("udp4");
+const connections = new NodeCache({ stdTTL: 60 });
 
+// #region Handlers
+function handleConnect(remote) {
+    const id = crypto.randomUUID();
+    connections.set(id, { address: remote.address });
+
+    const message = Buffer.from(`CONNECTED\nYOU ${id}\n`, 'ascii');
+    server.send(message, remote.port, remote.address);
+}
+// #endregion
+
+// #region Server events
 server.on("listening", () => {
     console.info("Server started");
 });
 
 server.on("message", (data, remote) => {
-    server.send(data, remote.port, remote.address);
+    data = data
+        .toString("ascii")
+        .split("\n");
+
+    if (data[0].toUpperCase().startsWith("CONNECT")) {
+        return handleConnect(remote);
+    }
 });
+// #endregion
 
 server.bind(PORT, "0.0.0.0");
